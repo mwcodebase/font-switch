76ea5c22790dbdbd36bdca888ed30eefe13d1610
mwcodebase <mwaldron.personal@gmail.com>
2021-01-28 22:17:08 -0500

    updated for font

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,5 @@
-let changeColor = document.getElementById('changeColor');
+let changeFont = document.getElementById('changeFont');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+changeFont.onclick = function(element) {
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
 };
